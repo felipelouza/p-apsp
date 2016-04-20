8d8d3036e326672e42466c527e2a4570fa7531cc
Felipe A. Louza <felipelouza@gmail.com>
2016-04-20 13:54:59 -0300

    Updated tests.sh

--- tests.sh
+++ tests.sh
@@ -2,7 +2,8 @@
 
 #sources
 #A=(10000 50000 100000 150000 200000 250000 300000)
-A=(10 50 100 150 200 250 300)
+#A=(10000000 50000000 100000000 150000000 200000000 250000000 300000000)
+A=(781250 1562500 3125000 6250000 12500000 25000000 50000000)
 
 D=("all_ests" "reads") 
 ext=(".fasta" ".fastq") 
@@ -11,18 +12,18 @@ L=(1 10 20 30 40 50)
 T=(1 2 4 8 16 32)
 
 host="bug"
-#dir="/mnt/disk1/database/"
-dir="~/dataset/"
+dir="/mnt/disk1/database/"
+#dir="~/dataset/"
 exp="time"
 out="0"
 
 #######################################################################
 
 mkdir -p tests
-#make clean 
-#make
+make clean 
+make
 	
-for i in 0
+for i in 1
 do
 	
 	test=${D[$i]}
@@ -36,7 +37,7 @@ do
 		echo "########"	
 		echo "K=${A[$j]}"	
 	
-		for k in {0..5}
+		for k in 1
 		do
 		
 			echo "####"	
@@ -49,7 +50,7 @@ do
 				date >> tests/${test}/${host}.db.${A[$j]}.${exp}.txt
 	
 				echo " " >> tests/${test}/${host}.db.${A[$j]}.${exp}.txt
-				make run DIR=${dir} INPUT=${D}${ext} K=${A[$j]} OUTPUT=${out} L=${L[$k]} T=${T[$t]} >> tests/${test}/${host}.db.${A[$j]}.${exp}.txt
+				make run DIR=${dir} INPUT=${D[$i]}${ext[$i]} K=${A[$j]} OUTPUT=${out} L=${L[$k]} T=${T[$t]} >> tests/${test}/${host}.db.${A[$j]}.${exp}.txt
 			done
 		done
 	done
